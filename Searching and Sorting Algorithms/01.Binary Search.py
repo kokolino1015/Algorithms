e91a3743452c71c0d58ab025a860aa488291e55b
Kaloyan Nedelkov <nedelkovkaloian@gmail.com>
2022-08-02 20:00:55 +0300

    Commit Files

--- Searching and Sorting Algorithms/01.Binary Search.py
+++ Searching and Sorting Algorithms/01.Binary Search.py
@@ -1,11 +1,21 @@
-def find_number(numbers, index):
-    middle = len(numbers) // 2
-    if index > middle:
-        pass
-    else:
-        pass
+def binary_search(numbers, target):
+    left = 0
+    right = len(numbers) - 1
+    while left <= right:
+        mid_idx = (right + left)//2
+        mid_num = numbers[mid_idx]
+
+        if mid_num == target:
+            return mid_idx
+
+        if target > mid_num:
+            left = mid_idx + 1
+        else:
+            right = mid_idx - 1
+    return -1
 
 
-list_of_numbers = [int(x) for x in input().split(", ")]
-list_of_numbers.sort()
-print(find_number(list_of_numbers, 2))
+numbers = [int(x) for x in input().split()]
+target = int(input())
+
+print(binary_search(numbers, target))
